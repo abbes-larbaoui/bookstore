538ed87598262f352a89f85cc6140d9329bab292
abbes larbaoui <larbaouiabbes@gmail.com>
2024-10-09 17:57:16 +0100

    first commit

---
+++ src/main/java/dz/kyrios/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package dz.kyrios.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
