a24750b9602d6d95ffb2cf3470019485bf28745b
abbes larbaoui <larbaouiabbes@gmail.com>
2024-10-13 16:45:31 +0100

    config documentation folder

--- src/main/java/dz/kyrios/bookstore/config/documentation/OpenAPIConfig.java
+++ src/main/java/dz/kyrios/bookstore/config/documentation/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package dz.kyrios.bookstore.config;
+package dz.kyrios.bookstore.config.documentation;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
